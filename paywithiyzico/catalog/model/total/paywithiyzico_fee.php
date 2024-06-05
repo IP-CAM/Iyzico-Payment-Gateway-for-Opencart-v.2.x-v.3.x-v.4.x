b6d7ff5bd7836c9a4fb7ed6c7333d07fea0a6181
Tarık KAMAT <54081414+tarikkamat@users.noreply.github.com>
2024-06-05 14:54:04 +0300

    Update paywithiyzico_fee.php
    PaywithiyzicoFee
    
    Signed-off-by: Tarık KAMAT <54081414+tarikkamat@users.noreply.github.com>

--- paywithiyzico/catalog/model/total/paywithiyzico_fee.php
+++ paywithiyzico/catalog/model/total/paywithiyzico_fee.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace Opencart\Catalog\Model\Extension\iyzico\Total;
-class paywithiyzico extends \Opencart\System\Engine\Model{
+namespace Opencart\Catalog\Model\Extension\paywithiyzico\Total;
 
+use Opencart\System\Engine\Model;
+
+class PaywithiyzicoFee extends Model
+{
     public function confirm($order_info, $order_total)
     {
-
+        return true;
     }
 
 }
